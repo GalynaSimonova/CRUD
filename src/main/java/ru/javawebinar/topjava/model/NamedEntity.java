7722b4d657a27c5e384194004d627fface029a4a
GalynaSimonova <simonov.galyna@gmail.com>
2018-10-05 17:06:14 +0300

    Add project structure Add spring

---
+++ src/main/java/ru/javawebinar/topjava/model/NamedEntity.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.model;
+
+public class NamedEntity extends BaseEntity {
+    protected String name;
+
+    public NamedEntity() {
+    }
+
+    protected NamedEntity(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-public class UserMeal {
+public class UserMeal extends BaseEntity {
     private final LocalDateTime dateTime;
 
     private final String description;
