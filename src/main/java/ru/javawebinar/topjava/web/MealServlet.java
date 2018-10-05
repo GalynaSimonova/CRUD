435017e71673181bad98d775d77d714d8d12e38f
GalynaSimonova <simonov.galyna@gmail.com>
2018-10-05 11:07:46 +0300

    Add logging

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -8,15 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserServlet extends HttpServlet {
-    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+public class MealServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(MealServlet.class);
 
     //protected void doPost(HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     //}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to userList");
-        response.sendRedirect("userList.jsp");
+        LOG.debug("redirect to mealList");
+        response.sendRedirect("mealList.jsp");
     }
 }
